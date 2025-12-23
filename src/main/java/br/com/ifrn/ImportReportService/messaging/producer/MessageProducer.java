ef826d2ae696dabdec9af007e4d4c9431ea091b9
ferreira.lima1 <ferreira.lima1@escolar.ifrn.edu.br>
2025-12-23 14:11:54 -0300

    feat(security): refactor security configuration and add oauth2-client
    - Added spring-boot-starter-oauth2-client dependency
    - Relocated SecurityConfig to br.com.ifrn.ImportReportService.config.security
    - Enabled @EnableWebSecurity and started filter chain refactor
    - Removed legacy "permit all" configuration for API endpoints

--- src/main/java/br/com/ifrn/ImportReportService/messaging/producer/MessageProducer.java
+++ src/main/java/br/com/ifrn/ImportReportService/messaging/producer/MessageProducer.java
@@ -1,12 +1,22 @@
 package br.com.ifrn.ImportReportService.messaging.producer;
 
 import br.com.ifrn.ImportReportService.config.rabbitmq.RabbitMQConfig;
+import br.com.ifrn.ImportReportService.config.security.SecurityContextService;
 import br.com.ifrn.ImportReportService.messaging.dto.ImportMessageDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageProducer {
+
+    @Autowired
+    SecurityContextService  securityContextService;
+
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -14,8 +24,17 @@ public class MessageProducer {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    public void sendMessage(ImportMessageDTO message) {
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "planilha.class-service-producer", message);
+    public void sendMessage(ImportMessageDTO payload) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                "planilha.class-service-producer",
+                payload, // 1. Primeiro passamos o dado (DTO)
+                message -> { // 2. Depois editamos o "envelope" (Headers)
+                    String realUserId = securityContextService.getCurrentUserId();
+                    message.getMessageProperties().setHeader("X-ORIGIN-USER-ID", realUserId);
+                    return message;
+                }
+        );
     }
 
     public void sendError(ImportMessageDTO message) {
